fee38b3629f4b97ce2a75c231ed7f6ce1339df62
Damian Freeman <38473158+TogamiCookiie@users.noreply.github.com>
2020-07-11 02:16:14 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # file is to remain outside of a folder, unless you're wanting to make a sub-folder
 
-mkdir ProjectName
-cd ProjectName
+mkdir ProjectName # for normal setup, set the name to lowercase so that npm init runs properly
+cd ProjectName # update name accordingly to the mkdir cmd
 npm init -y
